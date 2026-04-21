88019a31e57362ccfea31d0f1fd51cd380b388f4
Гофман Юрий Александрович <gfmwebpost@gmail.com>
2026-04-21 14:00:57 +0500

    игнор + начало модулей

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Modules\Product\Infrastructure\Providers\ProductServiceProvider;
+use App\Modules\ProductCategory\Infrastructure\Providers\ProductCategoryServiceProvider;
 use App\Providers\AppServiceProvider;
 
 return [
     AppServiceProvider::class,
+    ProductServiceProvider::class,
+    ProductCategoryServiceProvider::class,
 ];
